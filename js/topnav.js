f58401917bd0931ca8af99569e487258b1fa72b0
JONATHAN LEE <jon.leesc2@gmail.com>
2022-10-13 21:19:11 -0700

    idk whats wrong with the javascript it should work.
    JONonONFASONDFOANON

--- js/topnav.js
+++ js/topnav.js
@@ -1,9 +1,10 @@
 console.log("init");
 function myFunction(){
-  var x=document.getElementById("myHeroMenu");
-  if (x.className === "HeroMenu") {
+  var x=document.getElementById("mySideNav");
+  if (x.className === "sidenav") {
   x.className += " responsive";
 } else {
-  x.className = "HeroMenu";
+  x.className = "sidenav";
   }
 }
+/* taken from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_topnav*/
